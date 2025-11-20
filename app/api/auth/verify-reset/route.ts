af019f67add0f3ae8374b67f63b0d1dc06b12a41
Dhruv Roy <dhruv@capcons.com>
2025-11-20 14:16:10 +0530

    maxAge update for reset token

--- app/api/auth/verify-reset/route.ts
+++ app/api/auth/verify-reset/route.ts
@@ -57,7 +57,7 @@ export async function POST(request: Request) {
         secure: process.env.NODE_ENV === 'production',
         sameSite: 'none',
         path: '/',
-        maxAge: 15 * 60, // 15 minutes
+        maxAge: 5 * 60, // 15 minutes
       });
     }
 
